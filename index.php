c5fc20c0d082a42d209f303a794918ff701bb4c0
Soulstorm <teddy.cozette@hotmail.fr>
2024-05-24 19:57:13 +0200

    lien menant vers oeuvre-14 changer en oeuvre-14.php sans erreur

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
                 </a>
             </article>
             <article class="oeuvre">
-                <a href=<?php echo "oeuvre-13.php"; ?>>
+                <a href=<?php echo "oeuvre-14.php"; ?>>
                     <img src="img/steve-johnson-4.png" alt="Mirage">
                     <h2>Mirage</h2>
                     <p class="description">Stéphanie Kaiser</p>
