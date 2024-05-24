e09d03f35dc24c93d0823263bc2968ef5d9d43c4
Soulstorm <teddy.cozette@hotmail.fr>
2024-05-24 19:58:02 +0200

    oeuvre-15 modifier en oeuvre-15.php + intégration du header et footer

---
+++ oeuvre-15.php
@@ -0,0 +1,18 @@
+<!-- Intégration du Doctype + balise : head - body - header -->
+<?php require_once 'header.php'; ?>
+<main>
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="img/steve-johnson-3.png" alt="Blaue Gelbe Muster">
+        </div>
+        <div id="contenu-oeuvre">
+            <h1>Blaue Gelbe Muster</h1>
+            <p class="description">Adelheid Von Schreiber</p>
+            <p class="description-complete">
+                Curabitur dui odio, porta vel tempor sed, consectetur vitae mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus nec.
+            </p>
+        </div>
+    </article>
+</main>
+    <!-- Intégration du footer + fermeture des balise body et HTML -->
+    <?php require_once 'footer.php'; ?>
