c8cdd50f7877aa8611456d6d20fd98d1ee64ccfa
Soulstorm <teddy.cozette@hotmail.fr>
2024-05-24 17:53:57 +0200

    création des dossier header et footer .php + code de ceux-ci pour effectuer le remplacement

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- footer + balise fermante de bas de page -->
+<footer>
+        <p>
+            <strong>© THE ARTBOX</strong> - <em>Tous droits réservés</em>
+        </p>
+    </footer>
+</body>
+</html>
