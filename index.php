8a0ec2956f4a04076d1c8641737ee26acc411dfa
Soulstorm <teddy.cozette@hotmail.fr>
2024-05-25 19:21:42 +0200

    test de modification de l'url pour aller vers page oeuvre désiré

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
         <div id="liste-oeuvres">
             <!-- Intégration du fichier oeuvres.php pour faire appele a la $oeuvres -->
             <?php require_once 'oeuvres.php'; ?>
-            
+
             <!-- Boucle "pour chaque" qui affiche les articles avec les élements du tableau correspondant -->
             <?php foreach ($oeuvres as $oeuvre) : ?>
                 <article class="oeuvre">
-                    <a href="oeuvre-<?php echo ($oeuvre['id']); ?>.php">
+                    <a href="oeuvre.php?id=<?php echo ($oeuvre['id']); ?>">
                     <img src=<?php echo ($oeuvre['img']); ?> alt=<?php echo ($oeuvre['title']); ?>>
                     <h2><?php echo ($oeuvre['title']); ?></h2>
                     <p class='description'><?php echo ($oeuvre['description']); ?></p>
