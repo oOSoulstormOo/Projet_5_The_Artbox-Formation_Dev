05d0274dc3230855a04d82031b9a56ad7930b9b6
Soulstorm <teddy.cozette@hotmail.fr>
2024-05-25 19:12:17 +0200

    écriture du scipt php qui permet a la page de trouvé l'id qui lui a été envoyé et afficher les element en fonction de celle ci, a tester

--- oeuvre.php
+++ oeuvre.php
@@ -2,18 +2,35 @@
 <?php require_once 'header.php'; ?>
 
 <main>
+    <!-- Le fichier oeuvres.php qui possède la $oeuvres contenant notre tableau -->
+    <?php require_once 'oeuvres.php'; ?>
+    <!-- On vérifie si l'ID est présent dans l'URL -->
+    <?php if (isset($_GET['id'])) {
+            $id = $_GET['id]'];
+            $oeuvretrouve = null; 
+    };
+    ?>
+    <!-- Si il est présent, on cherche l'oeuvre correspondante a cette id dans le tableau -->
+    <?php foreach ($oeuvres as $oeuvre) {
+            if ($oeuvre['id'] === $id) {
+                $oeuvretrouve = $oeuvre;// on retourne la $oeuvretrouve comme étant l'oeuvre voulu
+                break;// et on arrête de chercher
+            }
+    }; ?>
+
+    <!-- Si l'oeuvre est trouvé, on affiche les détails de celle-ci -->
+    <?php if ($oeuvretrouve) : ?>
     <article id="detail-oeuvre">
         <div id="img-oeuvre">
-            <img src="img/clark-van-der-beken.png" alt="Dodomu">
+            <img src=<?php echo ($oeuvretrouve['img']); ?> alt=<?php echo ($oeuvretrouve['title']); ?>>
         </div>
         <div id="contenu-oeuvre">
-            <h1>Dodomu</h1>
-            <p class="description">Mia Tozerski</p>
-            <p class="description-complete">
-                Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-            </p>
+            <h1><?php echo ($oeuvretrouve['title']); ?></h1>
+            <p class="description"><?php echo ($oeuvretrouve['description']); ?></p>
+            <p class="description-complete"><?php echo ($oeuvretrouve['complete_description']); ?></p>
         </div>
     </article>
+    <?php endif; ?>
 </main>
     <!-- Intégration du footer + fermeture des balise body et HTML -->
     <?php require_once 'footer.php'; ?>
